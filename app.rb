9f49d649b9e3f63b108397948f1f2e4b8976a939
Jared Bears <97640061+JaredBears@users.noreply.github.com>
2023-08-05 01:56:09 +0000

    Implemented API Functionality

--- app.rb
+++ app.rb
@@ -1,9 +1,53 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "http"
+require "json"
+
+base_url = "https://api.exchangerate.host"
+
+symbols = nil
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  if symbols.nil?
+    symbols = get_symbols(base_url)
+  end
+  @symbols = symbols
+  erb :from
+
+end
+
+get("/:from") do
+  if symbols.nil?
+    symbols = get_symbols(base_url)
+  end
+  @symbols = symbols
+  @from = params[:from]
+
+  erb :to
+
+end
+
+get("/:from/:to") do
+
+  @from = params[:from]
+  @to = params[:to]
+  @result = convert_currency(base_url, @from, @to)
+
+  erb :results
+
+end
+
+def get_symbols(base_url)
+
+  response = JSON.parse(HTTP.get("#{base_url}/symbols"))
+  return response["symbols"].keys
+
+end
+
+def convert_currency(base_url, from, to)
+
+  response = JSON.parse(HTTP.get("#{base_url}/convert?from=#{from}&to=#{to}"))
+  return response["result"]
+
 end
